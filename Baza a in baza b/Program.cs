7c4ab3b59a2bd62693396b25e518f2f159488e7b
Adelina Manole <manoleadelina28@gmail.com>
2025-01-24 21:40:43 +0200

    Add project files.

---
+++ Baza a in baza b/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baza_a_in_baza_b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
